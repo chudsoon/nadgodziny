02a9214830e8e57120276d1ed926c39f7ff9ade7
ccc <ccc@cc>
2017-02-20 18:24:42 +0100

    główne okno (frame)

--- nadgodziny/src/nadgodziny/GUI.java
+++ nadgodziny/src/nadgodziny/GUI.java
@@ -1,8 +1,18 @@
 package nadgodziny;
 
-public class GUI {
+public class GUI extends Main {
 	
 	public GUI() {
+		frame();
+		
+	}
+	
+	private void frame() {
+		
+		setTitle("Nadgodziny");
+		setSize(500, 500);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 	}
 
---
+++ nadgodziny/src/nadgodziny/Main.java
@@ -0,0 +1,12 @@
+package nadgodziny;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+	
+	public static void main (String[] args) {
+		GUI gui = new GUI();
+		gui.setVisible(true);
+	}
+
+}
